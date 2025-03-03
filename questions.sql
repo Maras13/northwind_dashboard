d6ec8062c1940ec0f172a8c82cbf16a0ec1ff99c
Maras13 <maras.sara13@gmail.com>
2025-03-03 13:56:34 +0100

    added questions

---
+++ questions.sql
@@ -0,0 +1,15 @@
+Throughout the week write SQL queries to answer the questions below
+Get the names and the quantities in stock for each product.
+Get a list of current products (Product ID and name).
+Get a list of the most and least expensive products (name and unit price).
+Get products that cost less than $20.
+Get products that cost between $15 and $25.
+Get products above average price.
+Find the ten most expensive products.
+Get a list of discontinued products (Product ID and name).
+Count current and discontinued products.
+Find products with less units in stock than the quantity on order.
+Find the customer who had the highest order amount
+Get orders for a given employee and the according customer
+Find the hiring age of each employee
+Create views and/or named queries for some of these queries
